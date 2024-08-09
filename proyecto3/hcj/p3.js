ae49e2ef78ddca11c05a50dda7daae2fbf5b49a5
jorgegago <jorgegago2526@gmail.com>
2024-08-09 15:56:32 +0200

    p3 v1

--- proyecto3/hcj/p3.js
+++ proyecto3/hcj/p3.js
@@ -1,14 +1,10 @@
-const menu =  document.querySelector(".menu")
+const corner =  document.querySelector(".corner")
+// const menu = document.querySelector(".menu")
 const footer = document.querySelector("footer")
 const container = document.querySelector(".move-container")
 const unrotate = document.querySelectorAll(".org-tp-rg")
 
 const handleClick = (e) => {
-    // console.log("click menu")
-    // container.classList.toggle("rotate")
-    // footer.classList.toggle("rotate")
-    // unrotate.classList.toggle("unrotate")
-
     toggleClass({
         elements: [ container, footer],
         className: "rotate"
@@ -19,16 +15,16 @@ const handleClick = (e) => {
         className: "unrotate"
     })
 
-    // console.log(unrotate)
+    toggleClass({
+        elements: [corner],
+        className: "on-menu"
+    })
 }
 
-const toggleClass = ({ elements = [], className = " rotate"}) => {
+const toggleClass = ({ elements = [], className = "rotate"}) => {
     elements.map( x => {
         x.classList.toggle(`${className}`)
     } )
 }
 
-menu.addEventListener("click", handleClick)
-
-console.log(unrotate)
-
+corner.addEventListener("click", handleClick)
