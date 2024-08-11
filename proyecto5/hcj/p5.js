8c6b8385b0240ae2dd4b416027b2f1bf609ec602
jorgegago <jorgegago2526@gmail.com>
2024-08-11 19:09:29 +0200

    p5 v1 blur

---
+++ proyecto5/hcj/p5.js
@@ -0,0 +1,33 @@
+let num = 0
+let blu = 1
+const numbers = document.querySelector(".number")
+const blurr = document.querySelector("img")
+
+const timer = setInterval(
+    () => {
+        if (num === 100){
+            clearInterval(timer)
+            numbers.style.opacity = 0
+            blurr.style.filter = `blur(0rem)`
+            return 0
+        }  
+        num += 1
+        console.log(num)
+        setNum()
+        setBlurr()
+    }   
+,10)
+
+const setNum = () => {
+    numbers.innerHTML = `${num}%`
+    numbers.style.opacity = 1  - (0.007 * num)
+
+}
+
+const setBlurr = () => {
+    blu -= 0.01 
+    blurr.style.filter = `blur(${blu}rem)`
+}
+
+console.log(numbers, blurr)
+
