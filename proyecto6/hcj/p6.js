90f8a136213c8eacd186092ae2f85887090f8248
jorgegago <jorgegago2526@gmail.com>
2024-08-14 00:14:17 +0200

    p6 v1.0

--- proyecto6/hcj/p6.js
+++ proyecto6/hcj/p6.js
@@ -1,49 +1,6 @@
-// const zone = document.querySelector(".tets")
-// zone.elementFromPoint(50, 50)
-
 const boxes = document.querySelectorAll(".box")
-
-// const isVisible = (e) => {
-//     boxes.forEach(box => {
-//         box.classList.toggle("act", box.isIntersecting)
-//     })
-    
-// }
-
-// // const addObserver = (objs) => {
-// //     objs.forEach(element => {
-// //         observer.observe(element)
-// //     });
-// // }
-
-// const observer = new IntersectionObserver(isVisible, {rootMargin: "10%"})
-// // const observer = new IntersectionObserver(elements => {
-// //     elements.forEach(box => {
-// //         box.target.classList.toggle("act", box.isIntersecting)
-// //         console.info(box)
-// //     })
-// // },
-// // {
-// //     rootMargin: "0px",
-// // })
-// // addObserver(boxes)
-
-
-
-
-// const trigger = window.innerHeight * 0.9
-
-
-// const handleEvent = (e) => {
-//     // console.log("scroll")
-
-//     // const trigger = window.innerHeight * 0.9
-//     console.log(trigger)
-
-
-// }
-
 const trigger = window.innerHeight * 0.7
+
 const handleScroll = () => {
     boxes.forEach(box => {
         const Top = box.getBoundingClientRect().top
